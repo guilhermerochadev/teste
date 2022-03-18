6457915fbaf5c45e380d3719a21c2adcb9a9da4a
guilhermerochadev <guilhermerochadev@gmail.com>
2022-03-17 23:24:09 -0300

    Configuração das dependências no arquivo pom.xml e configuração do WebClient no application

--- src/main/java/br/com/hdi/test/dev/HdiTestDevApplication.java
+++ src/main/java/br/com/hdi/test/dev/HdiTestDevApplication.java
@@ -2,9 +2,29 @@ package br.com.hdi.test.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class HdiTestDevApplication {
+	
+	@Bean
+	public WebClient webClientProdutos(WebClient.Builder builder) {
+		return builder
+			.baseUrl("https://607732991ed0ae0017d6a9b0.mockapi.io/insurance/v1/broker")
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.build();
+	}
+	
+	@Bean
+	public WebClient webClientBrokerData(WebClient.Builder builder) {
+		return builder
+			.baseUrl("https://607732991ed0ae0017d6a9b0.mockapi.io/insurance/v1/brokerData")
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HdiTestDevApplication.class, args);
